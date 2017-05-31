96e416cd9b53e7201aef4b34c8c8da2af49710f3
ketaki <ketaki.nazirkar95@gmail.com>
2017-05-31 14:33:50 +0530

    runtimechangesinXml

---
+++ portrate/src/main/java/com/example/portrate/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.portrate;
+
+import android.content.res.Configuration;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this, "onRestart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            ((ImageView) findViewById(R.id.imgVw)).setImageResource(R.drawable.index);
+
+        }else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT){
+            ((ImageView) findViewById(R.id.imgVw)).setImageResource(R.drawable.img);
+        }else{
+            ((ImageView) findViewById(R.id.imgVw)).setImageResource(R.drawable.logo);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show();
+    }
+}
+
+
+
