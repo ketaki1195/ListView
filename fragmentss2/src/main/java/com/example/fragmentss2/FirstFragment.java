652c68828773d5d932d6a3dc37c8c4cef8422be9
ketaki <ketaki.nazirkar95@gmail.com>
2017-06-01 14:39:22 +0530

    fragments in android

---
+++ fragmentss2/src/main/java/com/example/fragmentss2/FirstFragment.java
@@ -0,0 +1,41 @@
+package com.example.fragmentss2;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FirstFragment extends Fragment {
+    public static final String KEY_FRAG_NUM = "fragNum";
+
+
+    public FirstFragment() {
+        // Required empty public constructor
+    }
+    public static FirstFragment getInstance(int image) {
+        Bundle bundle = new Bundle();
+        bundle.putInt(KEY_FRAG_NUM, image);
+        FirstFragment fragment = new FirstFragment();
+        fragment.setArguments(bundle);
+        return fragment;
+          }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        final View rootView = inflater.inflate(R.layout.fragment_first, container, false);
+        Bundle bundle = getArguments();
+              int img = bundle.getInt(KEY_FRAG_NUM);
+              ImageView imageView = (ImageView) rootView.findViewById(R.id.imageView);
+              imageView.setImageResource(img);
+              return rootView;
+          }
+
+}
